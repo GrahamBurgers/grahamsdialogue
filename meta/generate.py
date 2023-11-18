c404dab61c694c2798b5176b05ee4f0e59159f52
Nahtan <nathan.titirangi@gmail.com>
2023-11-19 09:48:43 +1300

    some font acts

--- meta/generate.py
+++ meta/generate.py
@@ -1,20 +1,30 @@
-path = "C:/Users/natha/Documents/code/noitadat/data/fonts/font_pixel_big.xml"
+path = "D:/Steam/steamapps/common/Noita/mods/grahamsdialogue/files/font_data/font_pixel_huge.xml"
+
 with open(path, "r") as f:
     text = f.read()
-chars = [chr(int(x.split("\"")[1])) for x in text.split("id")[1:]]
-widths = [int(x.split("\"")[1]) for x in text.split("width")[1:]]
+print([x.split("\"")[1] for x in text.split("id")[1:]])
+chars = [chr(int(x.split("\"")[1])) for x in text.split("id=")[1:]]
+print(chars)
+widths = [int(x.split("\"")[1]) for x in text.split("width=")[1:]]
 stuff = {}
 for a, b in zip(chars, widths):
     stuff[a] = b
-stuff["\\\""] = stuff["\""]
-stuff.pop("\"")
-stuff["\\\\"] = stuff["\\"]
-stuff.pop("\\")
+try:
+    stuff["\\\""] = stuff["\""]
+    stuff.pop("\"")
+except:
+    pass
+try:
+    stuff["\\\\"] = stuff["\\"]
+    stuff.pop("\\")
+except:
+    pass
 o = "return {"
 for k, v in stuff.items():
     o += f"[\"{k}\"] = {v},\n"
 o = o[:-2]
 o = o + "\n}"
 print(o)
-with open("out.lua", "w") as f:
+import codecs
+with codecs.open("out.lua", "w","utf-8") as f:
     f.write(o)
