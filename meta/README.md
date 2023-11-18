a405e94407cd080a88adafecd2f3f894f50c90d7
Nahtan <nathan.titirangi@gmail.com>
2023-11-18 17:15:50 +1300

    create the generator

---
+++ meta/README.md
@@ -0,0 +1,4 @@
+# This is a special folder for nathan font generator stuff.
+Remember to add this to dont_upload_folders when mod gets released
+File paths are specifically for me because idc
+Outputs to meta/out.lua, move to some special font folder
---
+++ meta/generate.py
@@ -0,0 +1,20 @@
+path = "C:/Users/natha/Documents/code/noitadat/data/fonts/font_pixel_huge.xml"
+with open(path, "r") as f:
+    text = f.read()
+chars = [chr(int(x.split("\"")[1])) for x in text.split("id")[1:]]
+widths = [int(x.split("\"")[1]) for x in text.split("width")[1:]]
+stuff = {}
+for a, b in zip(chars, widths):
+    stuff[a] = b
+stuff["\\\""] = stuff["\""]
+stuff.pop("\"")
+stuff["\\\\"] = stuff["\\"]
+stuff.pop("\\")
+o = "return {"
+for k, v in stuff.items():
+    o += f"[\"{k}\"] = {v},\n"
+o = o[:-2]
+o = o + "\n}"
+print(o)
+with open("out.lua", "w") as f:
+    f.write(o)
