c315e4498b4742337351bc8b8947211c93c5bde3
Graham <catface238@gmail.com>
2023-12-15 14:03:54 -0500

    kolmi kolmi kolmi

--- files/custom/kolmi_generic.lua
+++ files/custom/kolmi_generic.lua
@@ -5,18 +5,18 @@ dofile_once("mods/grahamsdialogue/files/types.lua")
 local phase = GlobalsGetValue("grahamsdialogue_kolmi_phase", "none")
 ---@type table<string,kolmi_pair>
 local phase_lines = { -- weight is 0..1
-	firepillar = {lines = { "Grr so fire!" }, weight = 1.0},
-	circleshot = {lines = { "Circles!!!" }, weight = 1.0},
-	chase = {lines = { "I am running...." }, weight = 1.0},
-	slow_chase = {lines = { "speakers", "write these grahams" }, weight = 1.0},
-	chase_direct = {lines = { "speakers", "write these grahams" }, weight = 1.0},
-	spawn_minion = {lines = { "speakers", "write these grahams" }, weight = 1.0},
-	explosion = {lines = { "speakers", "write these grahams" }, weight = 1.0},
-	homingshot = {lines = { "speakers", "write these grahams" }, weight = 1.0},
-	polymorph = {lines = { "speakers", "write these grahams" }, weight = 1.0},
-	melee = {lines = { "speakers", "write these grahams" }, weight = 1.0},
-	clean_materials = {lines = { "speakers", "write these grahams" }, weight = 1.0},
-	aggro = {lines = { "speakers", "write these grahams" }, weight = 1.0},
+	firepillar        = {lines = { "This should prove to be quite scalding...", "I'm reaching my boiling point with you...!" }, weight = 0.4},
+	circleshot        = {lines = { "Don't get dizzy... This one might hurt.", "Here's something basic. Dodge it, or else..." }, weight = 0.5},
+	chase             = {lines = { "Don't you dare run away from this fight!", "I'm not done with you yet! Get back here!" }, weight = 0.8},
+	slow_chase        = {lines = { "No way out now. Fight like it means the world.", "Get over here. I've got a surprise..." }, weight = 0.8},
+	chase_direct      = {lines = { "I'm not letting you go!", "Oh, no, you don't!" }, weight = 1.0},
+	spawn_minion      = {lines = { "Helpers, go! Give 'em hell!", "Onward, minions! Fight for me..." }, weight = 0.6},
+	explosion         = {lines = { "Boom! Take that!", "Shower the arena in sparks!" }, weight = 1.0},
+	homingshot        = {lines = { "Slime is extremely conductive... Keep your distance!", "Bugs can be quite shocking, I've heard..." }, weight = 1.0},
+	polymorph         = {lines = { "Did you bring polymorph immunity? What a shame...", "If I were you... I'd not want this one to hit." }, weight = 1.0},
+	melee             = {lines = { "Die already! Just die!", "You're... Such a frustrating creature!" }, weight = 1.0},
+	clean_materials   = {lines = { "Got to keep this place clean...", "Ah, what a mess we've made..." }, weight = 0.6},
+	aggro             = {lines = { "Cut it out! I'm so tired of you...", "This has gone on for far too long!" }, weight = 1.0},
 }
 if phase == "none" then return end
 local kolmi = GetUpdatedEntityID()
